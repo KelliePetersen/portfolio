60261c62134875ae056d026c4050dc1de8aceef1
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 15:30:45 +1000

    create subheading component

---
+++ src/components/subheading.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const Subheading = styled.p`
+  font-size: 0.75rem;
+  word-spacing: 3px;
+  letter-spacing: 3px;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 100%;
+  font-weight: ${props => props.weight ? props.weight : "bold"};
+  background-color: ${props => props.color ? props.color : props.theme.primary};
+  background-image: ${props => props.color ? "" : props.theme.gradient};
+`
+
+export default Subheading;
