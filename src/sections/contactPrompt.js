7f7e78ab260f874d6519ad0e102c911c2ec668dd
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 17:07:04 +1000

    style contactPropt section

--- src/sections/contactPrompt.js
+++ src/sections/contactPrompt.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import Heading from "../components/heading"
 import Subheading from "../components/subheading"
 import Button from '../components/button'
+import Paragraph from '../components/paragraph'
 
 const Wrapper = styled.section`
   background-color: ${props => props.theme.lightBackground};
@@ -15,13 +16,23 @@ const Container = styled.div`
   text-align: center;
 `
 
+const Email = styled.a`
+  color: ${props => props.theme.color};
+  font-weight: bold;
+  text-decoration: none;
+`
+
 const ContactPrompt = () => {
   return (
     <Wrapper>
       <Container>
-        <Subheading weight="400" color="#999">JAVASCRIPT DEVELOPER</Subheading>
-        <Heading large as="h1">Bask in my total shocking tagline you.</Heading>
-        <Button to="/contact" primary>GET IN TOUCH</Button>
+        <Subheading weight="400" color="#999">CONTACT</Subheading>
+        <Heading style={{margin: "10px 0 20px"}}>Say Hello.</Heading>
+        <Paragraph>Let me know your thoughts. 
+        Feel free to contact me through social media or email me at 
+          <Email href="mailto:hello@kelliepetersen.com"> hello@kelliepetersen.com</Email>.
+        </Paragraph>
+        <Button to="/contact" light style={{marginTop: '15px'}}>GET IN TOUCH</Button>
       </Container>
     </Wrapper>
   )
