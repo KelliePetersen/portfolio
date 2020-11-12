421b71a5b43ba354a071ee5de610231bc365640a
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 13:17:35 +1000

    style layout with basic theme colors

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import styled from "styled-components"
-
+import styled, { ThemeProvider } from "styled-components"
 import Footer from "./footer"
 import Header from "./header"
 import "./layout.css"
 
+const theme = {
+  main: "mediumseagreen",
+  background: "#1a1a1a",
+  text: "white"
+};
+
+const Container = styled.div`
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background: ${props => props.theme.background};
+  color: ${props => props.theme.text};
+`
+
 const Wrapper = styled.div`
-  margin: 20px 30px;
   max-width: 1720px;
+  margin: 0 30px;
+  padding: 20px 0;
 `
 
 const Layout = ({ children }) => {
@@ -24,11 +38,15 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <Wrapper>
-      <Header />
-      <main>{children}</main>
-      <Footer siteTitle={data.site.siteMetadata?.title || `Kellie Petersen`} />
-    </Wrapper>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Wrapper>
+          <Header />
+          <main>{children}</main>
+          <Footer siteTitle={data.site.siteMetadata?.title || `Kellie Petersen`} />
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
   )
 }
 
