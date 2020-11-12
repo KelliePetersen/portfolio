fdf0027461d07251ed7b3384bb23ddf62b6481d7
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 14:36:28 +1000

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kellie Petersen`,
+    description: `Front End developer specialising in JavaScript, React, Gatsby and NextJS.`,
+    author: `Kellie Petersen`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
