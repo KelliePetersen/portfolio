5ffa6bbc3f00df32f532d04993df95a618d48e5b
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 16:32:57 +1000

    create projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import SEO from "../components/seo"
 import Layout from "../components/layout"
 import Hero from "../sections/hero"
 import About from "../sections/about"
+import Projects from "../sections/projects"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
     <About />
+    <Projects />
   </Layout>
 )
 
---
+++ src/sections/projects.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from "styled-components"
+import Heading from "../components/heading"
+import Subheading from "../components/subheading"
+import Paragraph from "../components/paragraph"
+import Button from '../components/button'
+
+const Wrapper = styled.div`
+  width: 100%;
+  margin: 70px 0;
+`
+
+const Projects = () => {
+  return (
+    <Wrapper>
+      projects
+    </Wrapper>
+  )
+}
+
+export default Projects;
