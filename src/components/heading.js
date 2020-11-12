473decf9f7f36042c83e21da0c332e794384e6fe
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 16:48:21 +1000

    add letterspacing to heading

--- src/components/heading.js
+++ src/components/heading.js
@@ -5,6 +5,7 @@ const Heading = styled.h2`
   font-size: ${props => props.large ? "2.25rem" : "2rem"};
   margin: ${props => props.large ? "30px 0 40px" : "20px 0 30px"};
   line-height: 1.35;
+  letter-spacing: 0.5px;
 `
 
 export default Heading;
