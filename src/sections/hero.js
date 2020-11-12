cbe3fc47c696b67c74e31d2d764a505408c7063d
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 15:59:33 +1000

    remove unused Link import

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from "gatsby"
 import styled from "styled-components"
 import Heading from "../components/heading"
 import Subheading from "../components/subheading"
