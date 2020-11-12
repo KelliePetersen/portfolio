e669a198a42043fe529137efa3fe3a05da74deb7
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 13:58:51 +1000

    position header and create header components

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,51 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+
+const Container = styled.header`
+  position: fixed;
+  top: ${props => props.theme.verticalMargin};
+  left: 0;
+  padding: 0 ${props => props.theme.horizontalMargin};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Logo = styled(Link)`
+  margin: 0;
+  text-decoration: none;
+  color: ${props => props.theme.text};
+`
+
+const Menu = styled.button`
+  width: 40px;
+  height: 50px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  position: relative;
+`
+
+const MenuBar = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  width: 100%;
+  height: 3px;
+  background-color: ${props => props.theme.text};
+`
+
+const Header = () => (  
+  <Container>
+    <Logo to="/">K</Logo>
+    <Menu>
+      <MenuBar />
+    </Menu>
+  </Container>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
