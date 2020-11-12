388f8d40cb9dcd011288314fd19241aa1cd7f806
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 12:22:32 +1000

    create layout for mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,16 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
+import Footer from "./footer"
 import Header from "./header"
 import "./layout.css"
+
+const Wrapper = styled.div`
+  margin: 20px 30px;
+  max-width: 1720px;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,25 +24,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <Wrapper>
+      <Header />
+      <main>{children}</main>
+      <Footer siteTitle={data.site.siteMetadata?.title || `Kellie Petersen`} />
+    </Wrapper>
   )
 }
 
