0750364d64dd73f27c0d7f215a869b39bcb087bd
KelliePetersen <kelliepetersen@outlook.com>
2020-11-12 16:29:09 +1000

    add skills grid to about section with placeholder images

--- src/sections/about.js
+++ src/sections/about.js
@@ -11,6 +11,16 @@ const Wrapper = styled.section`
   padding: 70px ${props => props.theme.horizontalMargin};
 `
 
+const Grid = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 1fr 1fr 1fr;
+  // grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  column-gap: 15px;
+  row-gap: 15px;
+  margin-top: 60px;
+`
+
 const About = () => {
   return (
     <Wrapper>
@@ -20,6 +30,23 @@ const About = () => {
       I value readable and maintainable code.</Paragraph>
       <Paragraph>My focus is vanilla <span>JavaScript</span> and <span>React</span>, including <span>Gatsby</span> and 
       <span> NextJS</span>. I use Node and Express for the backend. I'm also familiar with WordPress and PHP.</Paragraph>
+      <Grid>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
+      </Grid>
     </Wrapper>
   )
 }
