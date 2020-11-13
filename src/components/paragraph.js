f540623da7aecd1153c8248b041c2d929b2c21f1
KelliePetersen <kelliepetersen@outlook.com>
2020-11-13 16:45:38 +1000

    make paragraph size responsive

--- src/components/paragraph.js
+++ src/components/paragraph.js
@@ -1,10 +1,20 @@
 import styled from "styled-components"
+import { device } from "../global/mediaQueries"
 
 const Paragraph = styled.p`
   font-size: 1rem;
   line-height: 1.75;
   letter-spacing: 0.5px;
   color: ${props => props.theme.grey};
+  max-width: 500px;
+  
+  @media ${device.laptop} {
+    font-size: 1.125rem;
+  }
+  @media ${device.laptopL} {
+    font-size: 1.25rem;
+    max-width: 550px;
+  }
 
   & > span {
     color: ${props => props.theme.color};
