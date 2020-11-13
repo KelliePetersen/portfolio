d4c8dade801b7f15f974f75437a6712a902a54a9
KelliePetersen <kelliepetersen@outlook.com>
2020-11-13 12:15:45 +1000

    change secondary color and subheading color

--- src/components/subheading.js
+++ src/components/subheading.js
@@ -4,12 +4,8 @@ const Subheading = styled.p`
   font-size: 0.75rem;
   word-spacing: 3px;
   letter-spacing: 3px;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-size: 100%;
   font-weight: ${props => props.weight ? props.weight : "bold"};
-  background-color: ${props => props.color ? props.color : props.theme.primary};
-  background-image: ${props => props.color ? "" : props.theme.gradient};
+  color: ${props => props.color ? props.color : props.theme.secondary};
   margin: 0;
 `
 
