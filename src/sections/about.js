92873162775ac27d9b94a07c1f735185ca4a1f6e
KelliePetersen <kelliepetersen@outlook.com>
2020-11-13 17:19:05 +1000

    make about section responsive

--- src/sections/about.js
+++ src/sections/about.js
@@ -17,49 +17,76 @@ const Wrapper = styled.section`
     padding: 70px 50px;
   }
   @media ${device.laptop} {
-    padding: 70px 75px;
+    padding: 75px;
   }
   @media ${device.laptopL} {
     padding: 100px;
+  }
+  @media ${device.desktopL} {
+    padding: 150px 200px;
+  }
+`
+
+const Container = styled.div`
+  width: max-content;
+  max-width: 100%;
+  margin: 0 auto;
+  @media ${device.desktop} {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
   }
 `
 
 const Grid = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: 1fr 1fr 1fr;
-  // grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  max-width: 435px;
+  grid-auto-rows: 75px;
+  grid-template-columns: repeat(auto-fill, minmax(75px, 1fr));
   column-gap: 15px;
   row-gap: 15px;
   margin-top: 60px;
+  @media ${device.tablet} {
+    grid-auto-rows: 100px;
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+    max-width: 580px;
+  }
+  @media ${device.desktop} {
+    margin-left: 20px;
+  }
 `
 
 const About = () => {
   return (
     <Wrapper>
-      <Subheading>ABOUT ME</Subheading>
-      <Heading large>Hi, I'm Kellie.</Heading>
-      <Paragraph>I'm a <span>front-end developer</span> and hobbyist designer working at Parchot. 
-      I value readable and maintainable code.</Paragraph>
-      <Paragraph>My focus is vanilla <span>JavaScript</span> and <span>React</span>, including <span>Gatsby</span> and 
-      <span> NextJS</span>. I use Node and Express for the backend. I'm also familiar with WordPress and PHP.</Paragraph>
-      <Grid>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-        <div style={{width: "100%", height: "75px", backgroundColor: "#333"}}></div>
-      </Grid>
+      <Container>
+        <div>
+          <Subheading>ABOUT ME</Subheading>
+          <Heading large>Hi, I'm Kellie.</Heading>
+          <Paragraph>I'm a <span>front-end developer</span> and hobbyist designer working at Parchot. 
+          I value readable and maintainable code.</Paragraph>
+          <Paragraph>My focus is vanilla <span>JavaScript</span> and <span>React</span>, including <span>Gatsby</span> and 
+          <span> NextJS</span>. I use Node and Express for the backend. I'm also familiar with WordPress and PHP.</Paragraph>
+        </div>
+        <Grid>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+          <div style={{width: "100%", height: "100%", backgroundColor: "#333"}}></div>
+        </Grid>
+      </Container>
     </Wrapper>
   )
 }
