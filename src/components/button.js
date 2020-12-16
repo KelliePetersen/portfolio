6f136418ac55e5d7ef0797f2fe16ea26abe620f9
KelliePetersen <kelliepetersen@outlook.com>
2020-12-16 18:06:35 +1000

    add animation to button

--- src/components/button.js
+++ src/components/button.js
@@ -6,10 +6,10 @@ const Button = styled.a`
   position: relative;
   color: white;
   text-decoration: none;
-  // border: 3px solid ${props => props.theme.primary};
+  // border: 3px solid ${props => props.theme.tertiary};
   // border-image: ${props => props.theme.gradient};
   // border-image-slice: 1;
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme.tertiary};
   background-image: ${props => props.theme.gradient};
   background-size: 200%;
   background-position: right;
@@ -19,9 +19,22 @@ const Button = styled.a`
   font-weight: bold;
   letter-spacing: 1.75px;
   padding: 20px 40px;
+  transition: background 0s;
 
   @media ${device.tablet} {
     font-size: 1.125rem;
+  }
+
+  &:hover, &:focus {
+    transition: background 0.75s;
+    background-size: 300%;
+    background-position: left;
+    background-image: linear-gradient(to right, 
+      ${props => props.theme.secondary}, 
+      ${props => props.theme.tertiary} 30%,
+      ${props => props.theme.secondary} 50%,
+      ${props => props.theme.secondary}
+    )
   }
 `
 
