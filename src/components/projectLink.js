5b470bd591bb25e4cde7b2171ee52fb58d3cc97e
KelliePetersen <kelliepetersen@outlook.com>
2020-12-17 14:46:45 +1000

    add hover/focus effect to project links

--- src/components/projectLink.js
+++ src/components/projectLink.js
@@ -4,12 +4,13 @@ import { device } from "../global/mediaQueries"
 const ProjectLink = styled.a`
   display: inline-block;
   position: relative;
-  color: white;
+  color: ${props => props.theme.color};
   text-decoration: none;
   font-size: 1rem;
   font-weight: bold;
   letter-spacing: 1.75px;
   padding: 0 30px 0 0;
+  transition: color 0.25s;
 
   @media ${device.mobile} {
     position: absolute;
@@ -21,19 +22,28 @@ const ProjectLink = styled.a`
     right: 0;
   }
   @media ${device.laptopL} {
-    padding: 17.5px 30px 17.5px 0;
+    margin-top: 20px;
   }
   
   &::after {
     content: "";
     position: absolute;
     top: 50%;
-    right: -20px;
+    left: calc(100% - 20px);
     transform: translateY(-50%) scaleX(-1);
     width: 40px;
     height: 3px;
     background-color: ${props => props.theme.primary};
     background-image: ${props => props.theme.gradient};
+    transition: width 0.25s;
+  }
+
+  &:hover, &:focus {
+    color: ${props => props.theme.secondary};
+  }
+
+  &:hover::after, &:focus::after {
+    width: 50px;
   }
 `
 
