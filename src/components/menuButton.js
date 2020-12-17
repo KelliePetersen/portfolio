96d60dff423b2ed602cb7cd534862d420b8a49b0
KelliePetersen <kelliepetersen@outlook.com>
2020-12-17 15:35:55 +1000

    add hover/focus effects to menu button

--- src/components/menuButton.js
+++ src/components/menuButton.js
@@ -1,6 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
 import styled from "styled-components"
 import { device } from "../global/mediaQueries"
+
+const MenuBar = styled.span`
+  position: absolute;
+  right: 0;
+  top: ${props => props.top || "auto"};
+  bottom: ${props => props.bottom || "auto"};
+  transform: ${props => props.center ? "translateY(-50%)" : ""};
+  width: 100%;
+  height: 3px;
+  background-color: ${props => props.theme.color};
+  box-shadow: 0.5px 0.5px 1px black;
+  transition: all 0.25s;
+`
 
 const Menu = styled.button`
   width: 40px;
@@ -9,9 +22,27 @@ const Menu = styled.button`
   border: none;
   cursor: pointer;
   position: relative;
+  transition: transform 0.25s;
+  
   @media ${device.laptop} {
     display: none;
   }
+
+  &:hover {
+    transform: scale(1.1);
+  }
+  &:hover ${MenuBar} {
+    background-color: ${props => props.theme.secondary};
+  }
+  
+  ${(props) => props.focus && `
+    &:focus {
+      transform: scale(1.1);
+    }
+    &:focus ${MenuBar} {
+      background-color: ${props.theme.secondary};
+    }
+  `}
 
   ${({cross}) => cross && `
     & ${MenuBar}:nth-child(1) {
@@ -34,24 +65,17 @@ const Menu = styled.button`
   `}
 `
 
-const MenuBar = styled.span`
-  position: absolute;
-  right: 0;
-  top: ${props => props.top || "auto"};
-  bottom: ${props => props.bottom || "auto"};
-  transform: ${props => props.center ? "translateY(-50%)" : ""};
-  width: 100%;
-  height: 3px;
-  background-color: ${props => props.theme.color};
-  box-shadow: 0.5px 0.5px 1px black;
-  transition: all 0.25s;
-`
-
 const MenuButton = ({ modalOpen, setModalOpen }) => {
-  const handleModal = () => modalOpen ? setModalOpen(false) : setModalOpen(true)
+  const [modalFocus, setModalFocus] = useState(true)
+
+  const handleModal = () => {
+    setModalFocus(false)
+    modalOpen ? setModalOpen(false) : setModalOpen(true)
+  }
+  const handleBlur = () => setModalFocus(true)
 
   return (
-    <Menu type="button" aria-label="open menu" onClick={handleModal} cross={modalOpen}>
+    <Menu type="button" aria-label="open menu" onClick={handleModal} onBlur={handleBlur} focus={modalFocus} cross={modalOpen}>
       <MenuBar top="0" />
       <MenuBar top="50%" center />
       <MenuBar bottom="0" />
