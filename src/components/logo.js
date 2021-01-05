aafa5662a2daa83d51c4e7f7984cd72144bab6aa
KelliePetersen <kelliepetersen@outlook.com>
2021-01-05 10:14:38 -0600

    add new logo to header

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import styled from "styled-components"
+import LogoSVG from '../images/logo.svg'
 
 const LogoLink = styled.a`
   margin: 0;
-  text-decoration: none;
-  color: ${props => props.theme.color};
-  font-size: 3rem;
-  font-weight: bold;
-  font-family: ${props => props.theme.headingFont};
-  transition: color 0.25s;
-  &:hover, &:focus {
-    color: ${props => props.theme.secondary};
-  }
+  width: 55px;
+  height: 55px;
+  display: block;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 50px;
+  position: relative;
+`
+
+const Background = styled.span`
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 80%;
+  background-color: ${props => props.theme.background};
+  border-radius: 50%;
+  z-index: -1;
 `
 
 const Logo = ({ setModalOpen }) => (
-  <LogoLink href="#" onClick={() => setModalOpen(false)}>K</LogoLink>
+  <LogoLink href="#" style={{backgroundImage: `url(${LogoSVG})`}} aria-label="Logo home page" onClick={() => setModalOpen(false)}>
+    <Background />
+  </LogoLink>
 )
 
-export default Logo;
+export default Logo
