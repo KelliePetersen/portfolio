0d56797639edcea092ee70b4727fc55b1a5a1d3b
KelliePetersen <kelliepetersen@outlook.com>
2021-01-19 11:19:17 -0600

    change button color to gradient and add hover effect

--- src/components/button.js
+++ src/components/button.js
@@ -5,12 +5,13 @@ import { device } from "../global/mediaQueries"
 const Arrow = styled.span`
   width: 12px;
   height: 12px;
-  border: 4px solid ${props => props.theme.background};
+  border: 4px solid ${props => props.theme.color};
   border-left: 0px;
   border-bottom: 0px;
   border-radius: 3px;
   transform: rotate(45deg);
   margin-left: 15px;
+  transition: border-color 0.25s;
   @media ${device.tablet} {
     width: 14px;
     height: 14px;
@@ -23,7 +24,7 @@ const ButtonStyle = styled.a`
   align-items: center;
   width: max-content;
   position: relative;
-  color: ${props => props.theme.background};
+  color: ${props => props.theme.color};
   text-decoration: none;
   background-color: ${props => props.theme.color};
   border-radius: 5px;
@@ -33,7 +34,7 @@ const ButtonStyle = styled.a`
   font-weight: bold;
   letter-spacing: 1.5px;
   padding: 17.5px 22.5px 17.5px 25px;
-  transition: background 0.25s;
+  transition: color 0.25s, background-image 0.25s;
 
   @media ${device.tablet} {
     font-size: 1.125rem;
@@ -48,14 +49,34 @@ const ButtonStyle = styled.a`
     }
   `}
 
-  &:hover, &:focus {
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 5px;
     background-color: ${props => props.theme.secondary};
+    background-image: ${props => props.theme.gradient};
+    transition: opacity 0.25s;
   }
+
+  &:hover, &:focus {
+    color: ${props => props.theme.background};
+  }
+  &:hover ${Arrow}, &:focus ${Arrow} {
+    border-color: ${props => props.theme.background};
+  }
+  &:hover:before {
+    opacity: 0;
+  }
+
 `
 
 const Button = ({ children, href, modal, style }) => (
   <ButtonStyle href={href} modal={modal} style={style}>
-    {children}<Arrow />
+    <span style={{zIndex: "5"}}>{children}</span><Arrow />
   </ButtonStyle>
 )
 
