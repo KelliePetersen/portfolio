bf88f181c59a432910c7019c2b7a3ff582d54d21
KelliePetersen <kelliepetersen@outlook.com>
2021-01-19 10:12:23 -0600

    change button style

--- src/components/button.js
+++ src/components/button.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import styled from "styled-components"
 import { device } from "../global/mediaQueries"
-import ArrowIcon from '../images/arrow.svg'
 
 const Arrow = styled.span`
-  position: absolute;
-  right: 25px;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 30px;
-  height: 30px;
-  display: inline-block;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 30px;
-  background-image: url(${ArrowIcon});
-  margin-left: 10px;
-  opacity: 0;
-  transition: opacity 0.25s;
+  width: 14px;
+  height: 14px;
+  border: 4px solid ${props => props.theme.background};
+  border-left: 0px;
+  border-bottom: 0px;
+  border-radius: 3px;
+  transform: rotate(45deg);
+  margin-left: 20px;
 `
 
 const ButtonStyle = styled.a`
@@ -25,22 +18,17 @@ const ButtonStyle = styled.a`
   align-items: center;
   width: max-content;
   position: relative;
-  color: white;
+  color: ${props => props.theme.background};
   text-decoration: none;
-  // border: 3px solid ${props => props.theme.tertiary};
-  // border-image: ${props => props.theme.gradient};
-  // border-image-slice: 1;
-  background-color: ${props => props.theme.tertiary};
-  background-image: ${props => props.theme.gradient};
-  background-size: 200%;
-  background-position: right;
+  background-color: ${props => props.theme.color};
   border-radius: 5px;
   box-shadow: 5px 5px 5px rgba(0,0,0,0.3);
+  font-family: ${props => props.theme.headingFont};
   font-size: 1rem;
   font-weight: bold;
-  letter-spacing: 1.75px;
-  padding: 22.5px 50px 22.5px 50px;
-  transition: background 0s, padding 0.25s;
+  letter-spacing: 1.5px;
+  padding: 22.5px 30px 22.5px 40px;
+  transition: background 0.25s;
 
   @media ${device.tablet} {
     font-size: 1.125rem;
@@ -55,22 +43,7 @@ const ButtonStyle = styled.a`
   `}
 
   &:hover, &:focus {
-    padding: 22.5px 70px 22.5px 30px;
-    transition: background 0.75s, padding 0.25s;
-    background-size: 300%;
-    background-position: left;
-    background-image: linear-gradient(to right, 
-      ${props => props.theme.secondary}, 
-      ${props => props.theme.tertiary} 30%,
-      ${props => props.theme.secondary} 50%,
-      ${props => props.theme.secondary}
-    )
-  }
-
-  &:hover ${Arrow}, &:focus ${Arrow} {
-    visibility: visible;
-    opacity: 1;
-    transition: opacity 0.5s;
+    background-color: ${props => props.theme.secondary};
   }
 `
 
