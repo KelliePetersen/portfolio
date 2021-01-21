8532b4ff66506008b0828e3b64c49a89cc366397
KelliePetersen <kelliepetersen@outlook.com>
2021-01-21 10:44:44 -0600

    add new simple logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#1c1c1c`,
         theme_color: `#1c1c1c`,
         display: `minimal-ui`,
-        icon: `src/images/logo-black.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
