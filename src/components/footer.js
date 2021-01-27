bf381a0f04db812b2df3448287d99824bd1420ae
KelliePetersen <kelliepetersen@outlook.com>
2021-01-27 10:34:43 -0600

    change how grid is calculated and elements are positioned within it

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,7 @@ const Wrapper = styled.footer`
     align-items: center;
   }
   @media ${device.laptop} {
-    grid-template-columns: 38% auto;
+    grid-template-columns: 25% 25% auto;
     row-gap: 50px;
   }
 `
@@ -27,9 +27,17 @@ const LogoFooter = styled.div`
   }
 `
 
+const NavWrapper = styled.div`
+  display: none;
+  @media ${device.laptop} {
+    grid-column-start: span 2; 
+    display: block;
+  }
+`
+
 const Social = styled.div`
   @media ${device.laptop} {
-    grid-column-start span 2; 
+    grid-column-start: span 3; 
     margin-bottom: 10px;
   }
 `
@@ -40,9 +48,12 @@ const Copyright = styled.p`
   text-align: center;
   letter-spacing: 0.5px;
   line-height: 1.35;
-
+  
   @media ${device.tablet} {
     text-align: left;
+  }
+  @media ${device.laptop} {
+    grid-column-start: span 2;
   }
 
   & > span {
@@ -56,7 +67,7 @@ const Copyright = styled.p`
 const Footer = () => (
   <Wrapper>
     <LogoFooter><Logo /></LogoFooter>
-    <Nav />
+    <NavWrapper><Nav /></NavWrapper>
     <Social><SocialMedia /></Social>
     <Copyright>© Copyright {new Date().getFullYear()} &bull; Kellie Petersen. <span>All Rights Reserved.</span></Copyright>
     <BackToTop style={{ margin: "0 0 0 auto" }}/>
