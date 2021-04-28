53896dcb332640c84fffafe887212e029cc9ae31
KelliePetersen <kelliepetersen@outlook.com>
2021-04-28 15:01:58 +1000

    apply styling of labels and inputs

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -11,18 +11,47 @@ const Wrapper = styled.div`
   }
 `
 
+const ContactLabel = styled.label`
+  display: block;
+  position: relative;
+  bottom: -15px;
+  color: ${props => props.required ? "red" : "#ccc"};
+  font-size: 0.875em;
+  letter-spacing: 0.5px;
+  transition: bottom 0.3s;
+`
+
+const ContactInput = styled.input`
+  display: block;
+  width: 100%;
+  max-width: 450px;
+  color: white;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  margin-bottom: 20px;
+
+  &:hover {
+    border-color: white;
+  }
+  &:focus {
+    border-color: ${props => props.theme.secondary};
+  }
+`
+
 const ContactForm = () => (
   <Wrapper>
     <form method="post">
-      <label htmlFor="name">Name
-        <input type="text" name="name" id="name" required/>
-      </label>
-      <label htmlFor="email">Email
-        <input type="email" name="email" id="email" required/>
-      </label>
-      <label htmlFor="message">Message
+      <ContactLabel htmlFor="name">Name
+        <ContactInput type="text" name="name" id="name" required/>
+      </ContactLabel>
+      <ContactLabel htmlFor="email">Email
+        <ContactInput type="email" name="email" id="email" required/>
+      </ContactLabel>
+      <ContactLabel htmlFor="message">Message
         <textarea name="message" id="message" rows="5" required></textarea>
-      </label>
+      </ContactLabel>
       <Button as="button" type="submit">SEND MESSAGE</Button>
     </form>
   </Wrapper>
