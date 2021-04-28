ce18909c9259b88ae5a0d1637b1d0a86ff5ef103
KelliePetersen <kelliepetersen@outlook.com>
2021-04-28 19:41:55 +1000

    use starBackground component in hero section

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -7,7 +7,7 @@ import Subheading from "../components/subheading"
 import Button from "../components/button"
 import { device } from "../global/mediaQueries"
 import SocialMedia from "../components/socialMedia"
-import img from "../images/stars100.svg";
+import StarBackground from "../components/starBackground"
 
 const Wrapper = styled.div`
   position: relative;
@@ -26,22 +26,6 @@ const Wrapper = styled.div`
   }
   @media ${device.widescreen} {
     max-height: 1500px;
-  }
-`
-
-const StarBackground = styled.div`
-  background-image: url(${img});
-  background-repeat: no-repeat;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  opacity: 0.5;
-  background-size: cover;
-  @media ${device.desktopL} {
-    background-position: center;
-    background-size: initial;
   }
 `
 
